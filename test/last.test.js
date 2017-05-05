80579dd25a2de324d76291ed2d97a0567a0de53f
Michael Clausen <michaeljclausen@gmail.com>
2017-05-04 21:20:46 -0700

    add tests for _.last()

--- test/last.test.js
+++ test/last.test.js
@@ -12,4 +12,12 @@ describe('last()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.last(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+
+  it('returns an empty array if you ask for zero elements', () => {
+    expect(_.last(['1','2','three'], 0)).toEqual([]);
+  });
+
+  it('returns an empty array if you input a negative n', () => {
+    expect(_.last([1,2,3], -2)).toEqual([]);
+  });
 });
