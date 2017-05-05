8320185b5f42fb68a80fd071f497fd0d723b3f78
Michael Clausen <michaeljclausen@gmail.com>
2017-05-05 12:44:27 -0700

    add tests for _.pluck()

--- test/pluck.test.js
+++ test/pluck.test.js
@@ -10,5 +10,24 @@ describe('pluck()', () => {
     const result = _.pluck(people, 'age');
     expect(result).toEqual([12, 999, 14]);
   });
+  
+  it('returns undefined values if a single object not in an array is passed', () => {
+    const person = {
+      name: 'Joe',
+      age: 42,
+      job: 'clerk'
+    };
+    const result = _.pluck(person, 'age');
+    expect(result).toEqual([undefined, undefined, undefined]);
+  });
 
+  it('returns an undefined value in the array if the object does not contain the key being plucked', () => {
+    const people = [
+      { name: 'Greg', age: 20},
+      { name: 'George'},
+      { name: 'Svetlana', age: 53}
+    ];
+    const result = _.pluck(people, 'age');
+    expect(result).toEqual([20, undefined, 53]);
+  });
 });
