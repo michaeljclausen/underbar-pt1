3d842eab21b62e249b18b57f2a24f727b91e08c7
Michael Clausen <michaeljclausen@gmail.com>
2017-05-05 15:56:44 -0700

    add tests for _.some()

--- test/some.test.js
+++ test/some.test.js
@@ -11,4 +11,14 @@ describe('some()', () => {
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
   });
 
+  it('returns true if any value in an object === \'a\'', () => {
+    const obj = {1: 1, 2: 'a', 3: undefined, 4: null};
+    expect(_.some(obj, val => val === 'a')).toBe(true);
+  });
+
+  it('returns false if no value in an object is null', () => {
+    const obj = {1: 1, 2: 'a', 3: undefined, 4: 'peanut butter'};
+    expect(_.some(obj, val => val === null)).toBe(false);
+  });
+
 });
