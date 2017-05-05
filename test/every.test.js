df77f0203b32c08e8021ef6048b9d50cf4b93bdf
Michael Clausen <michaeljclausen@gmail.com>
2017-05-05 15:48:35 -0700

    add tests for _.every()

--- test/every.test.js
+++ test/every.test.js
@@ -16,6 +16,15 @@ describe('every()', () => {
       const nums = [1, 3, 5, 6, 7];
       expect(_.every(nums, num => num % 2 === 1)).toBe(false);
     });
+  
+    it('returns true if all values in an object are of type string', () => {
+      const obj = {1: 'a', 2: 'b', 3: 'c'};
+      expect(_.every(obj, val => typeof val === 'string')).toBe(true);
+    });
 
+    it('returns false if not all values of an object are of type number', () => {
+      const obj = {1: 1, 2: '2', 3: 3};
+      expect(_.every(obj, val => typeof val === 'number')).toBe(false);
+    });
   });
 });
