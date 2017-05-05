33cba997288cce3b33075bce1b20606cfee3ff08
Michael Clausen <michaeljclausen@gmail.com>
2017-05-05 16:52:26 -0700

    add test for _.uniq()

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,9 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('de-dups a list of values in an object', () => {
+    const obj = {1:1, 2:1, 3:3};
+    expect(_.uniq(obj)).toEqual([1,3]);
+  });
 });
